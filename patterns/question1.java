ef73e19e8f22d03bba3c8719eee1e9368f319d22
rishabh2030 <rishabh.sharma1@ssipmt.com>
2024-03-12 05:04:36 +0530

    question 1 Solved

--- patterns/question1.java
+++ patterns/question1.java
@@ -1,7 +1,21 @@
 package patterns;
 
-class question1{
-    public static void main (String[] args) {
-        System.out.println("Rishabh");
+class question1 {
+    public static void main(String[] args) {
+        /*
+        *****
+        *****
+        *****
+        ***** 
+        */
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
+    // time complexity : O(n)
+    // space complexity : O(1)
 }
